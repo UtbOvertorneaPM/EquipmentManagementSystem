8b6a68213cd5929b7963db80835ed21e3b98e0ae
UtbOvertorneaPM <peter.mortberg@utb.overtornea.se>
2019-05-16 11:46:00 +0200

    Modified Owner model to add Date of creation

--- MacMS/Migrations/20190516093854_OwnerAdded.Designer.cs
+++ MacMS/Migrations/20190516093854_OwnerAdded.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using EquipmentManagementSystem.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EquipmentManagementSystem.Migrations
 {
     [DbContext(typeof(ManagementContext))]
-    partial class ManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20190516093854_OwnerAdded")]
+    partial class OwnerAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MacMS/Migrations/20190516093854_OwnerAdded.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EquipmentManagementSystem.Migrations
+{
+    public partial class OwnerAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Added",
+                table: "Owners",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Added",
+                table: "Owners");
+        }
+    }
+}
