4864327ed9471b35e0a199fd538448c633544a2b
UtbOvertorneaPM <peter.mortberg@utb.overtornea.se>
2019-09-13 08:33:33 +0200

    Cleaned up excessive javascript in create

---
+++ MacMS/wwwroot/js/CRUD.js
@@ -0,0 +1,13 @@
+﻿function RequestCreateOwnerModal(url) {
+
+    var phElement = $('#modal-placeholder');
+
+    $.get(url).done(function (data) {
+
+        phElement.html(data);
+        phElement.find('.modal').modal('show');
+    });
+
+    var x = document.getElementById("modal-placeholder");
+    x.style.display = "block";
+};
